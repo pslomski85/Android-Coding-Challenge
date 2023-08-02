ce4186f085c09d46df2ccaca13d7c7306fbbc99f
pslomski85 <piotrslomski@gmail.com>
2023-08-02 21:44:33 +0200

    Little menu cleanup

--- app/src/main/java/com/pixabay/viewer/screens/imagedetails/ImageDetailsFragment.kt
+++ app/src/main/java/com/pixabay/viewer/screens/imagedetails/ImageDetailsFragment.kt
@@ -2,17 +2,10 @@ package com.pixabay.viewer.screens.imagedetails
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.navArgs
 import com.pixabay.viewer.R
 import com.pixabay.viewer.databinding.FragmentImageDetailsBinding
@@ -39,23 +32,6 @@ class ImageDetailsFragment : Fragment() {
 
         imageLoader.loadImage(args.hit.largeImageURL, dataBinding.imageViewDetail)
 
-        setupMenu()
-
         return dataBinding.root
-    }
-
-    private fun setupMenu() {
-        val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                // Add menu items here
-                menuInflater.inflate(R.menu.empty_menu, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                // Handle the menu selection
-                return true
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
